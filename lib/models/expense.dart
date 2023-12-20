2f58cc60924153b5129ca6e0fc0dd2dba3a63a0b
teotia-codes <priyanshu.teotia50@gmail.com>
2023-12-20 21:00:18 +0530

    final commit

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,16 +1,19 @@
 import 'package:uuid/uuid.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-final formatter=DateFormat.yMd();
+
+final formatter = DateFormat.yMd();
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
-const categoryIcons={
+
+const categoryIcons = {
   Category.food: Icons.lunch_dining,
   Category.travel: Icons.flight_takeoff,
-  Category.leisure:Icons.movie,
+  Category.leisure: Icons.movie,
   Category.work: Icons.work,
 };
+
 class Expense {
   Expense(
       {required this.title,
@@ -23,7 +26,21 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
-  String get formattedDate{
+  String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+  ExpenseBucket.forCategory(List<Expense> allExpenses,this.category):expenses=allExpenses.where((expense) => expense.category==category).toList();
+  final List<Expense> expenses;
+  final Category category;
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
   }
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_app/chart/chart.dart';
 import 'package:expense_app/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_app/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
@@ -26,39 +27,73 @@ class _ExpensesState extends State<Expenses> {
   ];
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
-      builder: (ctx) =>  NewExpense(onAddExpense: _addExpense),
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
   }
-void _addExpense(Expense expense){
-  setState(() {
-    _registeredExpenses.add(expense);
-  });
-}
-void _removeExpense(Expense expense){
-  setState(() {
-    _registeredExpenses.remove(expense);
-  });
-}
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Expense Tracker'),
-        actions: [
-          IconButton(
-            onPressed: _openAddExpenseOverlay,
-            icon: const Icon(
-              Icons.add,
-            ),
-          ),
-        ],
-      ),
-      body: Column(children: [
-        const Text('The chart'),
-        Expanded(child: ExpensesList(expenses: _registeredExpenses, onRemoveExpense:_removeExpense ,)),
-      ]),
+    final width = MediaQuery.of(context).size.width;
+    Widget maincontent = const Center(
+      child: Text('No expense found , Start adding some!'),
     );
+    if (_registeredExpenses.isNotEmpty) {
+      maincontent = ExpensesList(
+          expenses: _registeredExpenses, onRemoveExpense: _removeExpense);
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Expense Tracker'),
+          actions: [
+            IconButton(
+              onPressed: _openAddExpenseOverlay,
+              icon: const Icon(
+                Icons.add,
+              ),
+            ),
+          ],
+        ),
+        body: width < 600
+            ? Column(children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(child: maincontent),
+              ])
+            : Row(
+                children: [
+                  Expanded(child: Chart(expenses: _registeredExpenses)),
+                  Expanded(child: maincontent),
+                ],
+              ));
   }
 }
